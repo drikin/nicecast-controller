9acad718288990b066977cd50d136e2660af04d4
Koh Aoki <drikin@gmail.com>
2015-04-05 11:15:32 -0700

    I hope this is the first working version

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
 var applescript = require('applescript');
 var chokidar    = require('chokidar');
 var fs          = require('fs');
+var os          = require('os');
 
 var lastSize = null;
 var filePath = null;
+var timer    = null;
+
+var targetPath = ".";
+if (process.argv[2]) {
+  targetPath = process.argv[2];
+  console.log(targetPath);
+}
 
 // One-liner for current directory, ignores .dotfiles
-chokidar.watch('.', {ignored: /[\/\\]\./}).on('add', function(path) {
+chokidar.watch(targetPath, {ignored: /[\/\\]\./}).on('add', function(path) {
   filePath = path;
   lastSize = fs.statSync(filePath).size;
 
-  console.log('Start broadcast');
   startBroadcast();
 });
 
 function check() {
+  console.log('Check');
+
   var size = fs.statSync(filePath).size;
   if (size !== lastSize) {
-    setTimeout(check, 10000);
+    lastSize = size;
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(check, 10000);
   } else {
     stopBroadcast();
   }
 }
 
 function startBroadcast() {
+  console.log('Start broadcast');
+
+  if (timer) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(check, 10000);
+
   var start = 'tell application "Nicecast" to start broadcast';
   asRun(start);
-
-  setTimeout(check, 10000);
 }
 
 function stopBroadcast() {
+  console.log('Stop broadcast');
+
   var stop = 'tell application "Nicecast" to stop broadcast';
   asRun(stop);
 }
 
 function asRun(script) {
+  if (os.platform !== "darwin") return;
+
   applescript.execString(script, function(err, rtn) {
     if (err) {
       // Something went wrong!
